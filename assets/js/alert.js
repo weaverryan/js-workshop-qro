d8d592e704c4725a8bede5765fcaf1759dc8b9bb
Ryan Weaver <ryan@thatsquality.com>
2020-08-14 14:04:30 -0400

    tuts hero working...

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -2,7 +2,7 @@ import '../css/alert.css';
 
 export default function (message) {
   const element = document.createElement('div');
-  element.classList.add('alert', 'alert-success', 'notification');
+  element.classList.add('alert', 'alert-success', 'mt-alert');
   element.innerHTML = `<span class="fa fa-lightbulb"></span> ${message}`;
   document.body.insertAdjacentElement('afterbegin', element);
 }
