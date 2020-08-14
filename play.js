6e691222387ce57bd90a538bfe2cf1a27ff62d28
Ryan Weaver <ryan@thatsquality.com>
2020-08-14 10:37:15 -0400

    tuts hero working...

--- play.js
+++ play.js
@@ -7,7 +7,11 @@ qro.printNameLater()
         console.log('callback!');
         console.log(data);
 
-        return data * 100;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(data * 100);
+            }, 2000);
+        });
     })
     .then((data) => {
         console.log(data);
