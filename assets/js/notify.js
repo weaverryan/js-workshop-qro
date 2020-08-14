c0b44625fbe3d4670481aec49bfcc537ed081017
Ryan Weaver <ryan@thatsquality.com>
2020-08-14 13:10:29 -0400

    tuts hero working...

--- assets/js/notify.js
+++ assets/js/notify.js
@@ -1,3 +1,6 @@
 export default function (message) {
-  alert(message);
-};
+  const element = document.createElement('div');
+  element.classList.add('alert', 'alert-success', 'm-0');
+  element.innerHTML = '<span class="fa fa-lightbulb"></span> ' + message;
+  document.body.insertAdjacentElement('afterbegin', element);
+}
