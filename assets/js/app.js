8907f3f4960dc5d1657c017a30bc6c6ac266a198
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:11:58 -0400

    Step 62: Alert Class: use new class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import sendAlert from './alert';
+import Alert from './alert';
 import $ from 'jquery';
 import '../css/app.css';
 
@@ -19,5 +19,6 @@ $('.js-comment-vote').on('click', async function (e) {
   });
 
   $container.find('.js-vote-total').text(data.votes);
-  sendAlert('Vote counted!');
+  const voteAlert = new Alert('Vote counted!');
+  voteAlert.render();
 });
