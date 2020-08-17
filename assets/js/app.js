56dd840d441d5608eae78bd53a513ec3e4b831a7
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:12:13 -0400

    Step 106: AsyncImport: check for comments in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
 import '../css/app.css';
 
 // future custom JS
+
+const comments = document.querySelectorAll('.js-comment-container');
+if (comments.length > 0) {
+  console.log('we should load the comments logic!');
+}
