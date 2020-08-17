c8eecc4a94466f28b23f087148a966b3d60db9bd
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:12:17 -0400

    Step 116: Stimulus: comment-out old code

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,11 +10,13 @@ application.load(definitionsFromContext(context));
 
 const comments = document.querySelectorAll('.js-comment-container');
 if (comments.length > 0) {
+  /*
   import('./components/comment').then(Comment => {
     comments.forEach(element => {
       new Comment.default(element);
     });
   });
+  */
 
   const submitAnswerButton = document.querySelector('.js-submit-answer');
   submitAnswerButton.addEventListener('click', () => {
