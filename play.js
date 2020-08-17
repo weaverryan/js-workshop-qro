5646894ca0cf7aaa6bf2be2609f75f2cb127964a
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:11:42 -0400

    Step 14: Promise: do something after

--- play.js
+++ play.js
@@ -3,4 +3,11 @@ const City = require('./city');
 const qro = new City('Querétaro');
 
 console.log(qro.name);
-qro.printStats();
+
+qro.printStats()
+  .then(() => {
+    console.log('callback after printStats()!');
+  })
+;
+
+console.log('End of script!');
