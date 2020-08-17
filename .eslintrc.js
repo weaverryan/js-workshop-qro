a6b236b775ca841c7b9af8bd8dc24313cb2ac218
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:11:51 -0400

    Step 44: ESLint: add config file

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const
+  globalDefinitions = {
+    // shortcuts of bootstrap $.notify(). symfony\web\js\main.js
+    bootstrapNotify: 'readonly',
+    showToast: 'readonly'
+  },
+
+  // 'off': 0  |  'warn': 1  |  'error': 2  (only the errors will crash the pipeline)
+  customRules = {
+    // allow semicolons ;
+    semi: 'off',
+
+    // allow prototype extending (useful for dates, strings...)
+    'no-extend-native': 'off',
+  };
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    jquery: true
+  },
+  extends: ['eslint:recommended', 'standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    ...globalDefinitions
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: customRules
+};
