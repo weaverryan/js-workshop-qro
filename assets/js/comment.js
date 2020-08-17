644fcddf17d29a52b5524d02f4c99052422bce34
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:12:06 -0400

    Step 87: Comments: attach click event

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,5 +1,13 @@
+import delegate from 'delegate';
+
 export default class {
   constructor (element) {
     this.element = element;
+
+    delegate(this.element, '.js-comment-vote', 'click', this.handleVoteClick.bind(this));
+  }
+
+  handleVoteClick (event) {
+    console.log('Clicked!');
   }
 }
