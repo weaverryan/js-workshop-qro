ef0109e113f053127b53415001be6f7ca7fbeb5d
Ryan Weaver <ryan@thatsquality.com>
2020-08-17 07:11:57 -0400

    Step 59: Better Alert: import CSS

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,6 +1,8 @@
+import '../css/alert.css';
+
 export default function (message) {
   const element = document.createElement('div');
-  element.classList.add('alert', 'alert-success');
+  element.classList.add('alert', 'alert-success', 'mt-alert');
   element.innerHTML = `<span class="fa fa-lightbulb"></span> ${message}`;
   document.body.insertAdjacentElement('afterbegin', element);
 }
